563b557f9e6cc87b67cd81173c464bb6c502e02c
MonkYan <monk.yan@gmail.com>
2020-07-16 23:08:55 +1200

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
 Route::get('/signup','UsersController@create')->name('signup');
+Route::resource('users','UsersController');
